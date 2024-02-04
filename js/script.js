6282cf10d0669eab73a6aa2a255b23547a57e6b1
sany0chekk <olexandr.veseluy@gmail.com>
2024-02-04 20:49:35 +0200

    v0.0.2
    - Add section aboud and header

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const burger = document.querySelector('[data-burger]');
+const burgerLink = document.querySelectorAll('[data-burger-link]');
+const burgerOpen = document.querySelector('[data-burger-open]');
+const burgerClose = document.querySelector('[data-burger-close]');
+
+burgerOpen.addEventListener('click', () => {
+	burger.classList.add('header-burger-active');
+	burgerLink.forEach((e, index) => {
+		setTimeout(() => {
+			e.classList.add('header-burger-item-active');
+		}, index * 200);
+	});
+});
+
+burgerClose.addEventListener('click', () => {
+	burger.classList.remove('header-burger-active');
+	burgerLink.forEach((e) => {
+		e.classList.remove('header-burger-item-active');
+	});
+});
